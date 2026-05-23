63118ce8984a0a9deb8dece11f0ac5c8f5cb85a2
nweerasuriya <nedeeshaw@virginmedia.com>
2026-05-23 17:15:34 +0100

    setup test suite

---
+++ CS7637/test_suite/CreatorArcData.py
@@ -0,0 +1,40 @@
+from copy import deepcopy
+import numpy as np
+
+
+class ArcData:
+    """
+    A Numpy multidimensional array representing an Arc Grid layout with data.
+
+    From an Arc problem perspective this represents a single grid of data
+    either input data or output data.
+    """
+    def __init__(self, data: np.ndarray):
+        self._arc_array: np.ndarray = np.array(data)
+
+    def data(self) -> np.ndarray:
+        """
+        Returns a copy of the numpy ndarray for this ArcData.
+        """
+        return self._arc_array
+
+    def reset_data(self, new_data: np.ndarray):
+        self._arc_array = np.array(new_data)
+
+    def shape(self) -> tuple[int, int]:
+        """
+        Returns the size of this numpy ndarray for this ArcData.
+        """
+        return self._arc_array.shape
+
+    def __eq__(self, other) -> bool:
+        """
+        Returns True if and only if the shape of
+        the other ndarray is the same as this
+        ArcData and all elements are equal;
+        False otherwise.
+        """
+        return np.array_equal(self.data(), other.data())
+
+    def to_list(self):
+        return self._arc_array.tolist()
---
+++ CS7637/test_suite/CreatorArcSet.py
@@ -0,0 +1,51 @@
+from CreatorArcData import ArcData
+
+
+class ArcSet:
+    """
+    An ArcProblem that has both an
+    input problem (ArcData) and a
+    corresponding output (ArcData)
+    for that particular input problem.
+
+    The output represents either a
+    transformation from the input
+    or the answer to a test problem.
+    """
+    def __init__(self, arc_input: ArcData, arc_output: ArcData = None):
+        self._input = arc_input
+        self._output = arc_output
+
+    def get_input_data(self) -> ArcData:
+        """
+        Returns the arc input
+        data for this set.
+        """
+        return self._input
+
+    def get_output_data(self) -> ArcData:
+        """
+        Returns the arc output
+        data for this set if
+        this set represents
+        training type data otherwise
+        this will return None.
+        """
+        return self._output
+
+    def __eq__(self, other) -> bool:
+        """
+        Returns True if and only if
+        both the input ArcData and
+        the output ArcData have
+        the same size and data;
+        False otherwise.
+        """
+        return (self.get_input_data().__eq__(other.get_input_data())
+                and self.get_output_data().__eq__(other.get_output_data()))
+
+    def to_dict(self):
+        data_dict: dict[str, list[int]] = dict()
+        data_dict['input'] = self._input.to_list()
+        data_dict['output'] = self._output.to_list()
+        return data_dict
